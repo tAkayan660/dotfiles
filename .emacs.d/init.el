594cd7d64c34d5ab20aa2b2788f942f380be4d79
tAkayan660 <s1526084PQ@s.chibakoudai.jp>
2015-11-24 10:10:53 +0900

    init.el 機能追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,9 +15,36 @@
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
 
+;; キーバインドの割り当て
 ;; C-c c でcompileコマンドを呼び出す
 (define-key mode-specific-map "c" 'compile)
 ;; C-c s でshell-commandを呼び出す
 (define-key global-map (kbd "C-c s") 'shell-command)
-;; C-c C-z でshellコマンドを呼び出す
+;; C-c C-s でshellコマンドを呼び出す
 (define-key global-map (kbd "C-c C-s") 'shell)
+;; C-m に newline-and-indent を割り当てる。
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; C-hをバックスペースに置き換える
+;; ?\C-? はDELのキーシーケンス
+(keyboard-translate ?\C-h ?\C-?)
+;; 別のキーバインドにヘルプを割り当てる。
+(global-set-key (kbd "C-x ?") 'help-command)
+;; 折り返しトグルコマンド
+(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t"でウィンドウを切り替える。初期値は transpose-chars
+(global-set-key (kbd "C-t") 'other-window)
+
+;; フレーム
+;; カラム番号も表示（行数）（モードライン）
+(column-number-mode t)
+;; 行数番号を常に表示
+(global-linum-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+;; (setq display-time-day-and-date t) ;　曜日・月・日を表示
+;; (display-time-mode t) ; 元
+;; (setq display-time-24hr-format t) ; 24時間表示
+(display-time-mode t)
+;; バッリー残量を表示
+(display-battery-mode t)
