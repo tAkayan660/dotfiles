d673c80f23dbd2c8f4462ec84ab2994081f4e9e7
tAkayan660 <s1526084PQ@s.chibakoudai.jp>
2015-12-14 01:38:27 +0900

    Flymake追加＆整理

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,9 @@
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
 
+;; yes or noをy or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
 ;; キーバインドの割り当て
 ;; C-c c でcompileコマンドを呼び出す
 (define-key mode-specific-map "c" 'compile)
@@ -39,9 +42,9 @@
 (column-number-mode t)
 ;; タイトルバーにファイルのフルパス表示
 (setq frame-title-format
-      (format "%%f - Emacs@%s" (system-name)))
-;; 行数番号を常に表示
-;; (global-linum-mode t)
+      (if (buffer-file-name)
+	  (format "%%f - Emacs")
+	(format "%%b - Emacs")))
 ;; 行番号表示
 (global-linum-mode t)
 (setq linum-format "%4d ")
@@ -171,3 +174,74 @@
 (elscreen-start)
 ;; タブの先頭に[X]を表示しない
 (setq elscreen-tab-display-kill-screen nil)
+
+;; Flymake の設定
+;; 参考
+;; http://d.hatena.ne.jp/sandai/20120304/p2
+(when (require 'flymake nil t)
+  ;; 下の一行はflymakeモードでエラー行に飛べるコマンドをキーに割り当ててるコードですが、
+  ;; 個人的な理由でコメントアウトしてます。必要でしたらこのコメント削除して、アンコメントしてください
+  ;;(define-key global-map (kbd "C-cd") 'flymake-display-err-menu-for-current-line)
+  
+  ;; C
+  ;; http://d.hatena.ne.jp/nyaasan/20071216/p1
+  (defun flymake-c-init ()
+    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+			 'flymake-create-temp-inplace))
+	   (local-file  (file-relative-name
+			 temp-file
+			 (file-name-directory buffer-file-name))))
+      (list "gcc" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+	       '("\\.\\(c\\|h\\|y\\l\\)$" flymake-c-init))
+  ;; C++
+  (defun flymake-cc-init ()
+    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+			 'flymake-create-temp-inplace))
+	   (local-file  (file-relative-name
+			 temp-file
+			 (file-name-directory buffer-file-name))))
+      (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+	       '("\\.cpp$" flymake-cc-init))
+  
+  ;; Emacs Lisp
+  ;; http://www.emacswiki.org/emacs/FlymakeElisp
+  (defun flymake-elisp-init ()
+    (unless (string-match "^ " (buffer-name))
+      (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+			   'flymake-create-temp-inplace))
+	     (local-file  (file-relative-name
+			   temp-file
+			   (file-name-directory buffer-file-name))))
+	(list
+	 (expand-file-name invocation-name invocation-directory)
+	 (list
+	  "-Q" "--batch" "--eval"
+	  (prin1-to-string
+	   (quote
+	    (dolist (file command-line-args-left)
+	      (with-temp-buffer
+		(insert-file-contents file)
+		(condition-case data
+		    (scan-sexps (point-min) (point-max))
+		  (scan-error
+		   (goto-char(nth 2 data))
+		   (princ (format "%s:%s: error: Unmatched bracket or quote\n"
+				  file (line-number-at-pos)))))))
+	    )
+	   )
+	  local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+	       '("\\.el$" flymake-elisp-init))
+
+  (add-hook 'emacs-lisp-mode-hook
+	    ;; workaround for (eq buffer-file-name nil)
+	    (function (lambda () (if buffer-file-name (flymake-mode)))))
+  (add-hook 'c-mode-common-hook
+	    (lambda () (flymake-mode t)))
+  (add-hook 'php-mode-hook
+	    (lambda () (flymake-mode t)))
+  )
+
+
--- .emacs.d/undohist/!home!taka!projects!dotfiles!.emacs.d!init.el
+++ .emacs.d/undohist/!home!taka!projects!dotfiles!.emacs.d!init.el
@@ -1,29 +1,29 @@
 
-((digest . "6ac134e17b928f895eb1d4fc5946d39d") (undo-list nil (4990 . 4991) (t 22125 11941 212386 999000) nil (#(";" 0 1 (fontified t face font-lock-comment-delimiter-face)) . -5082) nil (5080 . 5099) nil (5079 . 5080) (t 22125 11882 884385 388000) nil (5123 . 5124) nil (5120 . 5123) nil (5100 . 5120) nil (5080 . 5100) (t 22125 11554 156376 313000) nil (5059 . 5060) nil (5055 . 5056) nil (#("'" 0 1 (fontified t)) . -5055) nil (5055 . 5056) nil (5058 . 5059) nil (#("\"" 0 1 (face font-lock-string-face fontified t)) . -5058) nil (5054 . 5055) nil (#("\\" 0 1 (fontified t)) . 5054) nil (5050 . 5054) nil (#("\"" 0 1 (face font-lock-string-face fontified t)) . -5050) nil (#("c" 0 1 (fontified t)) . -5074) nil (5074 . 5075) (t 22125 11344 560370 527000) nil (5058 . 5074) nil (5057 . 5058) nil (5045 . 5057) nil (5025 . 5045) nil (5024 . 5025) nil (5005 . 5024) nil (#(";;; プレフィクスキーはC-t
-(setq elscreen-prefix-key (kbd \"C-c t\"))
-;;; タブの先頭に[X]を表示しない
-(setq elscreen-tab-display-kill-screen nil)
-;;; header-lineの先頭に[<->]を表示しない
-(setq elscreen-tab-display-control nil)
-;;; バッファ名・モード名からタブに表示させる内容を決定する(デフォルト設定)
-(setq elscreen-buffer-to-nickname-alist
-      '((\"^dired-mode$\" .
-	 (lambda ()
-	   (format \"Dired(%s)\" dired-directory)))
-	(\"^Info-mode$\" .
-	 (lambda ()
-	   (format \"Info(%s)\" (file-name-nondirectory Info-current-file))))
-	(\"^mew-draft-mode$\" .
-	 (lambda ()
-	   (format \"Mew(%s)\" (buffer-name (current-buffer)))))
-	(\"^mew-\" . \"Mew\")
-	(\"^irchat-\" . \"IRChat\")
-	(\"^liece-\" . \"Liece\")
-	(\"^lookup-\" . \"Lookup\")))
-(setq elscreen-mode-to-nickname-alist
-      '((\"[Ss]hell\" . \"shell\")
-	(\"compilation\" . \"compile\")
-	(\"-telnet\" . \"telnet\")
-	(\"dict\" . \"OnlineDict\")
-	(\"*WL:Message*\" . \"Wanderlust\")))
-" 0 4 (face font-lock-comment-delimiter-face fontified t) 4 17 (face font-lock-comment-face fontified t) 17 48 (fontified t) 48 55 (face font-lock-string-face fontified t) 55 58 (fontified t) 58 62 (face font-lock-comment-delimiter-face fontified t) 62 78 (face font-lock-comment-face fontified t) 78 122 (fontified t) 122 126 (face font-lock-comment-delimiter-face fontified t) 126 153 (face font-lock-comment-face fontified t) 153 193 (fontified t) 193 197 (face font-lock-comment-delimiter-face fontified t) 197 234 (face font-lock-comment-face fontified t) 234 283 (fontified t) 283 297 (face font-lock-string-face fontified t) 297 303 (fontified t) 303 309 (face font-lock-keyword-face fontified t) 309 325 (fontified t) 325 336 (face font-lock-string-face fontified t) 336 358 (fontified t) 358 371 (face font-lock-string-face fontified t) 371 377 (fontified t) 377 383 (face font-lock-keyword-face fontified t) 383 399 (fontified t) 399 409 (face font-lock-string-face fontified t) 409 458 (fontified t) 458 476 (face font-lock-string-face fontified t) 476 482 (fontified t) 482 488 (face font-lock-keyword-face fontified t) 488 504 (fontified t) 504 513 (face font-lock-string-face fontified t) 513 550 (fontified t) 550 557 (face font-lock-string-face fontified t) 557 560 (fontified t) 560 565 (face font-lock-string-face fontified t) 565 569 (fontified t) 569 579 (face font-lock-string-face fontified t) 579 582 (fontified t) 582 590 (face font-lock-string-face fontified t) 590 594 (fontified t) 594 603 (face font-lock-string-face fontified t) 603 606 (fontified t) 606 613 (face font-lock-string-face fontified t) 613 617 (fontified t) 617 627 (face font-lock-string-face fontified t) 627 630 (fontified t) 630 638 (face font-lock-string-face fontified t) 638 680 (fontified t) 680 689 (fontified t) 689 699 (face font-lock-string-face fontified t) 699 702 (fontified t) 702 709 (face font-lock-string-face fontified t) 709 713 (fontified t) 713 726 (face font-lock-string-face fontified t) 726 729 (fontified t) 729 738 (face font-lock-string-face fontified t) 738 742 (fontified t) 742 751 (face font-lock-string-face fontified t) 751 754 (fontified t) 754 762 (face font-lock-string-face fontified t) 762 766 (fontified t) 766 772 (face font-lock-string-face fontified t) 772 775 (fontified t) 775 787 (face font-lock-string-face fontified t) 787 791 (fontified t) 791 805 (face font-lock-string-face fontified t) 805 808 (fontified t) 808 820 (face font-lock-string-face fontified t) 820 824 (fontified t)) . 5005) (t 22123 62412 115328 673000) ((marker) . -824) ((marker) . -824) nil undo-tree-canary))
+((digest . "15db2d50160f3f711270d6aad8405071") (undo-list nil (#(";; initial frame maximize
+;; 参考
+;; http://d.hatena.ne.jp/sandai/20120304/p2
+
+;; 起動時にウィンドウ最大化
+;; http://www.emacswiki.org/emacs/FullScreen#toc12
+(defun jbr-init ()
+  \"Called from term-setup-hook after the default
+   terminal setup is
+   done or directly from startup if term-setup-hook not
+   used.  The value
+   0xF030 is the command for maximizing a window.\"
+  (interactive)
+  (w32-send-sys-command #xf030)
+  (ecb-redraw-layout)
+  (calendar))
+
+(let ((ws window-system))
+  (cond ((eq ws 'w32)
+	 (set-frame-position (selected-frame) 0 0)
+	 (setq term-setup-hook 'jbr-init)
+	 (setq window-setup-hook 'jbr-init))
+	((eq ws 'ns)
+	 ;; for MacBook Air(Late2010) 11inch display
+	 (set-frame-position (selected-frame) 0 0)
+	 (set-frame-size (selected-frame) 95 47))))
+" 0 3 (fontified t face font-lock-comment-delimiter-face) 3 26 (fontified t face font-lock-comment-face) 26 29 (fontified t face font-lock-comment-delimiter-face) 29 32 (fontified t face font-lock-comment-face) 32 35 (fontified t face font-lock-comment-delimiter-face) 35 76 (fontified t face font-lock-comment-face) 76 77 (fontified t) 77 80 (fontified t face font-lock-comment-delimiter-face) 80 93 (fontified t face font-lock-comment-face) 93 96 (fontified t face font-lock-comment-delimiter-face) 96 144 (fontified t face font-lock-comment-face) 144 145 (fontified t) 145 150 (fontified t face font-lock-keyword-face) 150 151 (fontified t) 151 159 (fontified t face font-lock-function-name-face) 159 165 (fontified t) 165 289 (fontified t face font-lock-doc-face) 289 359 (fontified t face font-lock-doc-face) 359 446 (fontified t) 446 449 (fontified t face font-lock-keyword-face) 449 474 (fontified t) 474 478 (fontified t face font-lock-keyword-face) 478 626 (fontified t) 626 629 (fontified t face font-lock-comment-delimiter-face) 629 670 (fontified t face font-lock-comment-face) 670 759 (fontified t)) . 7514) (t 22125 39757 577714 993000) ((marker) . -26) ((marker) . -26) ((marker) . -32) ((marker) . -32) ((marker) . -76) ((marker) . -76) ((marker) . -77) ((marker) . -77) ((marker) . -93) ((marker) . -93) ((marker) . -144) ((marker) . -144) ((marker) . -163) ((marker) . -163) ((marker . 7514) . -759) ((marker . 7514) . -759) ((marker) . -212) ((marker) . -212) ((marker) . -233) ((marker) . -233) ((marker) . -289) ((marker) . -289) ((marker) . -309) ((marker) . -309) ((marker) . -360) ((marker) . -360) ((marker) . -376) ((marker) . -376) ((marker) . -408) ((marker) . -408) ((marker) . -430) ((marker) . -430) ((marker) . -444) ((marker) . -444) ((marker) . -445) ((marker) . -445) nil (1399 . 1400) (#("	              " 0 15 (fontified t)) . -1399) 1404 nil (1373 . 1376) (#("		          " 0 12 (fontified t)) . -1373) 1387 nil (1344 . 1350) (#("            " 0 12 (fontified t)) . -1344) 1378 nil (1452 . 1453) nil (#(")" 0 1 (fontified t)) . -1452) nil (1441 . 1453) nil (1421 . 1441) nil (1413 . 1421) nil (1401 . 1413) nil (1381 . 1401) nil (1378 . 1381) nil (1370 . 1378) nil (1350 . 1370) nil (1343 . 1350) nil (1339 . 1343) nil (1319 . 1339) nil (#(";; (setq frame-title-format
+;;      (format \"%%f - Emacs@%s\" (system-name)))" 0 3 (face font-lock-comment-delimiter-face fontified t) 3 28 (face font-lock-comment-face fontified t) 28 36 (face font-lock-comment-delimiter-face fontified t) 36 76 (face font-lock-comment-face fontified t)) . 1319) (t 22125 39111 17684 148000)))
