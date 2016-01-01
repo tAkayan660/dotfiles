e40094aa69c8f4af6c3fc7954371e60d574191ca
tAkayan660 <s1526084PQ@s.chibakoudai.jp>
2016-01-02 02:43:01 +0900

    install.sh追加

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/tAkayan660/dotfiles.git
+cd dotfiles/.emacs.d/public_repos/
+git clone https://github.com/ancane/emacs-nav.git
+git clone https://github.com/auto-complete/popup-el.git
