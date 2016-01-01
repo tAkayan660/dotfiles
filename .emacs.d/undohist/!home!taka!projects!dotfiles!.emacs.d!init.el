8dd4fee8cdf60f93ff156f6f1ffc5ccc7145a532
tAkayan660 <s1526084PQ@s.chibakoudai.jp>
2016-01-01 21:31:00 +0900

    デーモン上でC-h（バックスラッシュ）の有効化

--- .emacs.d/undohist/!home!taka!projects!dotfiles!.emacs.d!init.el
+++ .emacs.d/undohist/!home!taka!projects!dotfiles!.emacs.d!init.el
@@ -1,7 +1,2 @@
 
-((digest . "fe3bf426b24497020d3a0443e28225ea") (undo-list nil (#("
-;; server start for emacs-client
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-" 0 1 (fontified t) 1 4 (fontified t face font-lock-comment-delimiter-face) 4 34 (fontified t face font-lock-comment-face) 34 35 (fontified t) 35 42 (fontified t face font-lock-keyword-face) 42 44 (fontified t) 44 50 (fontified t face font-lock-constant-face) 50 53 (fontified t) 53 59 (fontified t face font-lock-keyword-face) 59 97 (fontified t)) . 564) (t 22138 48769 741302 150000) ((marker . 964) . -1) ((marker . 964) . -1) ((marker . 943) . -34) ((marker . 943) . -34) ((marker . 894) . -52) ((marker . 894) . -52) ((marker . 858) . -79) ((marker . 858) . -79) ((marker . 811) . -97) ((marker . 811) . -97) ((marker . 564) . -97) ((marker . 564) . -97) nil undo-tree-canary))
+((digest . "b4c6d37821b76ae095997381635e7126") (undo-list nil (#("h" 0 1 (fontified t)) . -1135) (t 22150 21357 711224 995000) nil undo-tree-canary))
