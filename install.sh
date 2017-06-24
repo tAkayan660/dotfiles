abaebf938eb52065027ad253490ad823b761bb82
takayan660 <s1526084PQ@s.chibakoudai.jp>
2017-06-24 18:21:58 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/tAkayan660/dotfiles.git
 
 #emacs
-cd dotfiles/.emacs.d/public_repos/
+cd dotfiles/emacs.d/public_repos/
 git clone https://github.com/ancane/emacs-nav.git
 git clone https://github.com/auto-complete/popup-el.git
 git clone https://github.com/milkypostman/powerline.git
