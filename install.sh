a5b205530ac23d67a4a4f651c90c4387bbb467dd
takayan660 <s1526084PQ@s.chibakoudai.jp>
2017-10-09 18:57:50 +0900

    add python

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR=$(cd $(dirname $0) && pwd)/dotfiles
 
 #Package install
 sudo apt-get -q update
-sudo apt-get -y git vim bash zsh
+sudo apt-get -y git vim bash zsh python
 
 #dotfiles
 git clone https://github.com/tAkayan660/dotfiles.git
